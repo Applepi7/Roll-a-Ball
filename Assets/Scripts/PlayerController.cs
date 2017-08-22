d3a44c0173ee27d0b76374b4460ab22c990b1a4e
Applepi7 <dhwjddnjs2000@naver.com>
2017-08-22 11:47:28 +0900

    Player moving

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10.0f;
+
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+	void FixedUpdate () {
+        Moving();
+    }
+
+    void Moving()
+    {
+        float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        float moveVertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rigidbody.AddForce(movement * speed);
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+<h1> Roll a Ball </h1>
